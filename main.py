104e094bd7d9f57da2e07ee8bc1bb43263c87881
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:24:00 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-def manfiy_sonlar(N):
-    if N < 0:
-        return [i for i in range(1, N+1) if i < 0]
-    else:
-        return []
+def ortacha_soni_hesobla(sonlar):
+    natija = 0
+    for son in sonlar:
+        natija += son
+    ortacha = natija / len(sonlar)
+    return ortacha
 
-print(manfiy_sonlar(-10))
+sonlar_ro_yuqlandi = [1, 2, 3, 4, 5]
+print("Sonlar ro'yxati: ", sonlar_ro_yuqlandi)
+sonlar_ortachasi = ortacha_soni_hesobla(sonlar_ro_yuqlandi)
+print("Sonlar ro'yxati o'rtacha qiymati: ", sonlar_ortachasi)
+
+sonlar_ro_yuqlandi = [10, 20, 30, 40, 50]
+print("Sonlar ro'yxati: ", sonlar_ro_yuqlandi)
+sonlar_ortachasi = ortacha_soni_hesobla(sonlar_ro_yuqlandi)
+print("Sonlar ro'yxati o'rtacha qiymati: ", sonlar_ortachasi)
+
+sonlar_ro_yuqlandi = [100, 200, 300, 400, 500]
+print("Sonlar ro'yxati: ", sonlar_ro_yuqlandi)
+sonlar_ortachasi = ortacha_soni_hesobla(sonlar_ro_yuqlandi)
+print("Sonlar ro'yxati o'rtacha qiymati: ", sonlar_ortachasi)
